b61dd234004e0b9619f6a432ee587799b9a4ce25
Felipe <fpachecomanoel@uol.com.br>
2020-09-02 11:42:25 -0300

    more syntax correction

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -22,7 +22,7 @@
 	// byeSpeaker object instead of being a standalone function.
 	// See Lecture 52, part 2
 	window.byeSpeaker=byeSpeaker;
-}(window);
+})(window);
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
